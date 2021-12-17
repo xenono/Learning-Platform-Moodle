5517fe45ebf84aa97890149660d1df1e74c84131
xenono <aurbanczyk0@gmail.com>
2021-12-17 15:05:39 +0000

    Add Week dropdown to courses subpage.

--- views/courses.php
+++ views/courses.php
@@ -1,7 +1,21 @@
 <?php
 include "../components/header.php";
 ?>
-<h1>Courses</h1>
+<div class="wrapper">
+    <div class="grid-courses-view">
+        <div class="week-dropdown">
+            <h1>All weeks</h1>
+            <ul class="week-dropdown-list">
+                <?php for($i = 1; $i < 28; $i+=1){?>
+                    <li class="week-dropdown-list-item"><?php echo "Week " . $i ?></li>
+                <?php } ?>
+            </ul>
+        </div>
+        <div class="course-content">
+
+        </div>
+    </div>
+</div>
 <?php
 include "../components/footer.php";
 ?>
