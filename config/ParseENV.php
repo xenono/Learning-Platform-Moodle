437157a84f51a1b0f2f6b87ea3eca2078aedd2ab
Noyalbabu <20209749@hope.ac.uk>
2022-04-25 11:21:12 +0100

    Update ParseENV.php

--- config/ParseENV.php
+++ config/ParseENV.php
@@ -3,7 +3,7 @@ $env = fopen("../.env", "r") or die("Unable to open file!");
 while(!feof($env)){
     $line = fgets($env);
     $key = explode('=',$line)[0];
-    $value = explode('"',$line)[1];
+    $value = explode('"', explode("=",$line)[1])[1];
     $_ENV[$key] = $value;
 }
 fclose($env);
