5e3a4b6b558c2352743b3f8ee6ff09ce6241947a
xenono <aurbanczyk0@gmail.com>
2022-05-16 03:24:23 +0100

    Add styles for universityFaculty and courses. Finished dashboard styles.

--- views/index.php
+++ views/index.php
@@ -6,13 +6,11 @@ include("../includes/header.php");
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-    <div class = "index">
+    <div class = "index-wrapper">
         <h1>Welcome to Ace Training</h1>
         <div class = "nav">
             <a class= "nav-item"  href ="../views/login.php"> <i class = "fa fa-sign-in" aria-hidden="true"> Login </i></a>
             <a class= "nav-item"  href ="../views/signup.php"> <i class = "fa fa-user-plus" aria-hidden="true"> Sign Up </i></a>
-            <a class= "nav-item"  href ="../views/login.php"> <i class = "fa fa-sign-out" aria-hidden="true"> Login out </i></a>
-
         </div>
 
 
--- views/universityFaculty.php
+++ views/universityFaculty.php
@@ -16,15 +16,14 @@ if ($courses->num_rows > 0) {
     $courses = mysqli_fetch_all($courses);
 }
 ?>
-<body>
-
     <div class="unifac">
         <img src = "../public/assets/logo.png">
         <h1>Faculty</h1>
         <div class = "desc">
         <p>The ACE Training System is a premier online school that has attracted international notice for the high quality of education it provides. It is completely equipped with professionally educated tutors and a well-organized administrative staff. We'd like to introduce you to our tutors.</p>
         </div>
-        <div class="uni">
+
+        <div class="tutors">
 
         <?php
         $sql = "SELECT user.id AS Id , user.name AS name, user.surname AS surname, user.email AS email, user.phoneNumber AS number FROM tutor LEFT JOIN user ON 
@@ -32,25 +31,24 @@ if ($courses->num_rows > 0) {
         $result = mysqli_query($conn,$sql);
         if($result){
             while($row = $result->fetch_object()){?>
-            <table>
-            <td>
-                <?php echo "Name: <b>".$row->name." ".$row->surname."</b><br>"; ?>
-                <?php echo "Contact details: <b>".$row->email."</b> 
-                            <br><b>(+44)".$row->number."</b><br>"; ?>
-                <?php echo "Availability: <b> Monday to Friday, 9:00am - 9:00pm </b><br>";?>
-                <?php
-                echo "Courses :";
-                $sqlin = "SELECT course.courseName AS courseName FROM tutorcourse LEFT JOIN course ON tutorcourse.courseId = course.courseId WHERE tutorcourse.tutorId = $row->Id ";
-                $resultsqlin = mysqli_query($conn, $sqlin);
-                if($resultsqlin) {
-                    while ($dis = $resultsqlin->fetch_object()) {
-                        echo "<b>".$dis->courseName."</b><br>";
+            <div>
+                <p><?php echo "Name: ".$row->name." ".$row->surname ?></p>
+                <p>Contact details: <?= $row->email ?></p>
+                <p>Availability: Monday to Friday, 9:00am - 9:00pm</p>
+                <p style="font-weight: bold;">Courses:</p>
+                <ul>
+                    <?php
+                    $sqlin = "SELECT course.courseName AS courseName FROM tutorcourse LEFT JOIN course ON tutorcourse.courseId = course.courseId WHERE tutorcourse.tutorId = $row->Id ";
+                    $resultsqlin = mysqli_query($conn, $sqlin);
+                    if($resultsqlin) {
+                        while ($dis = $resultsqlin->fetch_object()) {
+                            echo "<li>".$dis->courseName."</li>";
+                        }
                     }
-                }
-                ?>
+                    ?>
+                </ul>
 
-            </td>
-            </table>
+            </div>
             <?php
             }
         }else{
@@ -60,4 +58,6 @@ if ($courses->num_rows > 0) {
 
         </div>
 </div>
-</body>
+<?php
+include("../includes/footer.php");
+?>
