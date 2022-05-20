c6e65a0cb0d161a2b55dd0bfe84e57865eca107a
TristinG21 <20207920@hope.ac.uk>
2022-05-20 15:17:16 +0100

    my work updated
    updated

---
+++ views/Profile/db.php
@@ -0,0 +1,8 @@
+<?php 
+$mysql_hostname = "localhost";
+$mysql_user = "username";
+$mysql_password = "";
+$mysql_database = "moodle";
+$con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
+?>
+
